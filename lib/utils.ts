6081f8bd50284199fe45ca168d45c77eab4a6ff1
Mikael Aditya N <mikaeladityan.99@gmail.com>
2024-10-22 13:59:40 +0700

    Setup: Architecture

---
+++ lib/utils.ts
@@ -0,0 +1,7 @@
+import { Plus_Jakarta_Sans } from "next/font/google";
+export const BACKEND = process.env.NEXT_PUBLIC_API;
+
+export const jakartaSans = Plus_Jakarta_Sans({
+	display: "swap",
+	subsets: ["latin"],
+});
